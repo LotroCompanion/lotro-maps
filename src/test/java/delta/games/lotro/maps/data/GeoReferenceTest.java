405e6c172dab1f145370ce9535fa059aa096a540
Damien Morcellet <dmorcellet@free.fr>
2024-09-22 20:34:20 +0200

    Use JUnit 5 instead of JUnit 3

--- src/test/java/delta/games/lotro/maps/data/GeoReferenceTest.java
+++ src/test/java/delta/games/lotro/maps/data/GeoReferenceTest.java
@@ -1,15 +1,17 @@
 package delta.games.lotro.maps.data;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.awt.Dimension;
 
-import junit.framework.Assert;
-import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
 
 /**
  * Geo conversions tests.
  * @author DAM
  */
-public class GeoReferenceTest extends TestCase
+class GeoReferenceTest
 {
   private static GeoPoint _startPoint=new GeoPoint(-114.01f, -3.826f);
   private static GeoReference _geoReference=new GeoReference(_startPoint, 7.704f*10);
@@ -17,24 +19,26 @@ public class GeoReferenceTest extends TestCase
   /**
    * Test conversion from geo to pixels.
    */
-  public void testConversionGeo2Pixel()
+  @Test
+  void testConversionGeo2Pixel()
   {
     GeoPoint naga=new GeoPoint(-105.8f, -7.5f);
     Dimension nagaPixels = _geoReference.geo2pixel(naga);
     System.out.println(nagaPixels);
-    Assert.assertEquals(632,nagaPixels.width);
-    Assert.assertEquals(283,nagaPixels.height);
+    assertEquals(632,nagaPixels.width);
+    assertEquals(283,nagaPixels.height);
   }
 
   /**
    * Test conversion from pixels to geo.
    */
-  public void testConversionPixel2Geo()
+  @Test
+  void testConversionPixel2Geo()
   {
     Dimension nagaPixels=new Dimension(632,283);
     GeoPoint naga=_geoReference.pixel2geo(nagaPixels);
     System.out.println(naga);
-    Assert.assertTrue(Math.abs(-105.8f-naga.getLongitude())<0.01);
-    Assert.assertTrue(Math.abs(-7.5f-naga.getLatitude())<0.01);
+    assertTrue(Math.abs(-105.8f-naga.getLongitude())<0.01);
+    assertTrue(Math.abs(-7.5f-naga.getLatitude())<0.01);
   }
 }
